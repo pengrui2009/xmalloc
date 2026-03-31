efc91b27b57b9f37f6f7a33dd3facf2f6d39f347
Rui Peng <1020314683@qq.com>
2026-03-31 18:44:03 +0800

    兼容C++语言

--- xmalloc.h
+++ xmalloc.h
@@ -1,13 +1,19 @@
 #ifndef XMALLOC_H
 #define XMALLOC_H
 
-
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 extern void xmalloc_init();
 extern void *xmalloc_hook(size_t size, const char* file, int line);
 extern void xfree_hook(void *ptr, const char* file, int line);
 extern void xmalloc_print() ;
 extern void xmalloc_deinit();
+
+#ifdef __cplusplus
+}
+#endif
 
 //宏定义实现代码中调用malloc/free时调用我们目标函数
 #define malloc(size)         xmalloc_hook(size, __FILE__, __LINE__)
